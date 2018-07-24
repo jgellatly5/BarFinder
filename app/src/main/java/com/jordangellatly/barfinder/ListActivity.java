2602892a608fb2c415d61408784075194d674869
Jordan Gellatly <jgellatly5@gmail.com>
2018-07-23 20:37:16 -0700

    ListView shows the first 20 queries from the Yelp API
    You can also go back to the Main Activity to search again

--- app/src/main/java/com/jordangellatly/barfinder/ListActivity.java
+++ app/src/main/java/com/jordangellatly/barfinder/ListActivity.java
@@ -1,13 +1,113 @@
 package com.jordangellatly.barfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.jordangellatly.barfinder.models.Bar;
+import com.jordangellatly.barfinder.models.Business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ListActivity extends AppCompatActivity {
+
+    private static final String TAG = "ListActivity";
+
+    @BindView(R.id.list_view)
+    ListView listView;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+
+    private static final String BASE_URL = "https://api.yelp.com/v3/";
+
+    private static final String API_KEY = "Bearer mUk4y0zvNI7RJ8coBuobzsTT8w0bGUMmauOciscQ-lB2SQfe7zZdco8FnlqU8BH1blaEfJJv_2TCVJh0excsjuDrdiyLtNSVecvccAk43xim0N2jyQY_Uxjj6C8eWHYx";
+
+    private static final String latitude = "37.801459";
+    private static final String longitude = "-122.26525579999998";
+
+    private Retrofit retrofit;
+    private YelpClient yelpClient;
+
+    private String category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        ButterKnife.bind(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        progressBar.setVisibility(ProgressBar.VISIBLE);
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+        OkHttpClient okHttpClient = builder.build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+
+        yelpClient = retrofit.create(YelpClient.class);
+
+        category = getIntent().getStringExtra("category");
+
+        Call<Bar> call = yelpClient.listBars(API_KEY, latitude, longitude, category);
+        call.enqueue(new Callback<Bar>() {
+            @Override
+            public void onResponse(Call<Bar> call, Response<Bar> response) {
+                Bar bar = response.body();
+                List<Business> businesses = bar.getBusinesses();
+                ArrayList<String> businessNames = new ArrayList<>();
+                for (Business singleBusiness : businesses) {
+                    businessNames.add(singleBusiness.getName());
+                }
+
+                ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(ListActivity.this, android.R.layout.simple_list_item_1, businessNames);
+                listView.setAdapter(listViewAdapter);
+                progressBar.setVisibility(ProgressBar.INVISIBLE);
+            }
+
+            @Override
+            public void onFailure(Call<Bar> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage());
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/jordangellatly/barfinder/MainActivity.java
+++ app/src/main/java/com/jordangellatly/barfinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jordangellatly.barfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,30 +8,25 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.jordangellatly.barfinder.models.Bar;
+import com.jordangellatly.barfinder.models.Business;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "MainActivity";
-    
+
     @BindView(R.id.intro_text)
     TextView introText;
     @BindView(R.id.type_spinner)
@@ -38,63 +34,26 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @BindView(R.id.btn_search)
     Button btnSearch;
 
-    private static final String BASE_URL = "https://api.yelp.com/v3/";
-
-    private static final String API_KEY = "Bearer mUk4y0zvNI7RJ8coBuobzsTT8w0bGUMmauOciscQ-lB2SQfe7zZdco8FnlqU8BH1blaEfJJv_2TCVJh0excsjuDrdiyLtNSVecvccAk43xim0N2jyQY_Uxjj6C8eWHYx";
-
-    private static final String latitude = "37.801459";
-    private static final String longitude = "-122.26525579999998";
-    
     private String category;
-
-    private Retrofit retrofit;
-    private YelpClient yelpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        Stetho.initializeWithDefaults(this);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.bar_types, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         typeSpinner.setAdapter(adapter);
         typeSpinner.setOnItemSelectedListener(this);
-
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-
-        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
-        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        builder.networkInterceptors().add(httpLoggingInterceptor);
-        OkHttpClient okHttpClient = builder.build();
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(okHttpClient)
-                .build();
-
-        yelpClient = retrofit.create(YelpClient.class);
     }
 
     @OnClick(R.id.btn_search)
     public void search() {
-        Call<Bar> call = yelpClient.listBars(API_KEY, latitude, longitude, category);
-        call.enqueue(new Callback<Bar>() {
-            @Override
-            public void onResponse(Call<Bar> call, Response<Bar> response) {
-
-
-            }
-
-            @Override
-            public void onFailure(Call<Bar> call, Throwable t) {
-                Log.e(TAG, "onFailure: " + t.getMessage());
-
-            }
-        });
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        intent.putExtra("category", category);
+        startActivity(intent);
     }
 
     @Override
